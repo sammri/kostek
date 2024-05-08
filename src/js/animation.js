58e2964a450bfe2f7633f9d6bb650a5215776b0f
sammri <ivildemort4837@gmail.com>
2024-05-08 23:16:44 +0200

    -

---
+++ src/js/animation.js
@@ -0,0 +1,87 @@
+//-------------- icons 3 ----------------//
+$(document).ready(function() {
+    $(".fact-icon").hover(
+        function() { 
+            $(this).css("transform", "scale(1.2)"); 
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
+
+//----------header - logo -----------//
+// $(document).ready(function() {
+//     $(".header-logo").hover(
+//         function() { 
+//             $(this).css("transform", "scale(1.2)"); 
+//         }, 
+//         function() { 
+//             $(this).css("transform", "scale(1)"); 
+//         }
+//     );
+// });
+
+//----------footer - logo -----------//
+
+$(document).ready(function() {
+    $(".logo-img").hover(
+        function() { 
+            $(this).css("transform", "scale(1.2)"); 
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
+
+//----------photo - item-------//
+$(document).ready(function() {
+    $(".photo-item").hover(
+        function() { 
+            $(this).css("transform", "scale(1.02)");    
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
+
+//----------hero - title-----//
+$(document).ready(function() {
+    $(".hero-title").hover(
+        function() { 
+            $(this).css("transform", "scale(1.1)"); 
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
+
+//--------project-photo portfolio-------//\
+
+$(document).ready(function() {
+    $(".projeckt").hover(
+        function() { 
+            $(this).css("transform", "scale(1.03)"); 
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
+
+
+
+//--------contact - us-----//
+$(document).ready(function() {
+    $(".contact-us").hover(
+        function() { 
+            $(this).css("transform", "scale(1.1)"); 
+        }, 
+        function() { 
+            $(this).css("transform", "scale(1)"); 
+        }
+    );
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,47 @@
-$(document).ready(function () {
-  $("#logo").hover(
-    function () {
-      $(this).css("transform", "scale(1.2)");
-    },
-    function () {
-      $(this).css("transform", "scale(1)");
-    }
-  );
+//validate form 
+$(document).ready(function(){
+  $('.form').submit(function(event) {
+      var name = $('input[name="name"]').val();
+      var email = $('input[name="email"]').val();
+      var message = $('input[name="message"]').val();
+      var isValid = true;
+
+      // Валідація імені
+      if (!name.trim()) {
+          isValid = false;
+          $('input[name="name"]').next('.error').text('Please enter your name');
+      } else {
+          $('input[name="name"]').next('.error').text('');
+      }
+
+      // Валідація email
+      if (!email.trim() || !isValidEmail(email)) {
+          isValid = false;
+          $('input[name="email"]').next('.error').text('Please enter a valid email address');
+      } else {
+          $('input[name="email"]').next('.error').text('');
+      }
+
+      // Валідація повідомлення
+      if (!message.trim()) {
+          isValid = false;
+          $('input[name="message"]').next('.error').text('Please enter your message');
+      } else {
+          $('input[name="message"]').next('.error').text('');
+      }
+
+      if (!isValid) {
+          event.preventDefault();
+      }
+  });
 });
+
+
+
+
+
+
+
+
+
+
